0ca748eb87587bf995977473f2b982328f0192e1
Jan Roguszka <jan@stacker.app>
2020-06-09 21:32:45 +0100

    added db

--- app/main/controller/post_controller.py
+++ app/main/controller/post_controller.py
@@ -35,5 +35,4 @@ class PostList(Resource):
 class Post(Resource):
     @api.doc('get the image')
     def get(self, filename):
-        print('hello')
         return send_from_directory(store, filename)
